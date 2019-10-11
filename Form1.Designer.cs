c5b96392ff3d5545c5f85222979cba731866e244
komok113 <abdulhakim.dev@gmail.com>
2019-10-11 21:21:25 +0300

    Second Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace New_Project_Voice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_Top = new System.Windows.Forms.Panel();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_resume = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_openfile = new System.Windows.Forms.Button();
+            this.btn_Record = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pnl_Top.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Top
+            // 
+            this.pnl_Top.BackColor = System.Drawing.Color.DarkViolet;
+            this.pnl_Top.Controls.Add(this.btn_Close);
+            this.pnl_Top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_Top.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Top.Name = "pnl_Top";
+            this.pnl_Top.Size = new System.Drawing.Size(863, 25);
+            this.pnl_Top.TabIndex = 0;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_start.FlatAppearance.BorderSize = 0;
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start.Image = ((System.Drawing.Image)(resources.GetObject("btn_start.Image")));
+            this.btn_start.Location = new System.Drawing.Point(11, 41);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(87, 46);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.Btn_start_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Close.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_Close.FlatAppearance.BorderSize = 0;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.Image = ((System.Drawing.Image)(resources.GetObject("btn_Close.Image")));
+            this.btn_Close.Location = new System.Drawing.Point(838, 0);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(25, 25);
+            this.btn_Close.TabIndex = 1;
+            this.btn_Close.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_Record);
+            this.panel1.Controls.Add(this.btn_pause);
+            this.panel1.Controls.Add(this.btn_openfile);
+            this.panel1.Controls.Add(this.btn_resume);
+            this.panel1.Controls.Add(this.btn_start);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(109, 522);
+            this.panel1.TabIndex = 2;
+            // 
+            // btn_resume
+            // 
+            this.btn_resume.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_resume.FlatAppearance.BorderSize = 0;
+            this.btn_resume.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_resume.Image = ((System.Drawing.Image)(resources.GetObject("btn_resume.Image")));
+            this.btn_resume.Location = new System.Drawing.Point(11, 145);
+            this.btn_resume.Name = "btn_resume";
+            this.btn_resume.Size = new System.Drawing.Size(87, 46);
+            this.btn_resume.TabIndex = 1;
+            this.btn_resume.UseVisualStyleBackColor = true;
+            this.btn_resume.Click += new System.EventHandler(this.Btn_resume_Click);
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_pause.FlatAppearance.BorderSize = 0;
+            this.btn_pause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_pause.Image = ((System.Drawing.Image)(resources.GetObject("btn_pause.Image")));
+            this.btn_pause.Location = new System.Drawing.Point(11, 93);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(87, 46);
+            this.btn_pause.TabIndex = 1;
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.Btn_pause_Click);
+            // 
+            // btn_openfile
+            // 
+            this.btn_openfile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_openfile.FlatAppearance.BorderSize = 0;
+            this.btn_openfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_openfile.Image = ((System.Drawing.Image)(resources.GetObject("btn_openfile.Image")));
+            this.btn_openfile.Location = new System.Drawing.Point(11, 197);
+            this.btn_openfile.Name = "btn_openfile";
+            this.btn_openfile.Size = new System.Drawing.Size(87, 46);
+            this.btn_openfile.TabIndex = 1;
+            this.btn_openfile.UseVisualStyleBackColor = true;
+            this.btn_openfile.Click += new System.EventHandler(this.Btn_openfile_Click);
+            // 
+            // btn_Record
+            // 
+            this.btn_Record.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Record.FlatAppearance.BorderSize = 0;
+            this.btn_Record.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Record.Image = ((System.Drawing.Image)(resources.GetObject("btn_Record.Image")));
+            this.btn_Record.Location = new System.Drawing.Point(11, 249);
+            this.btn_Record.Name = "btn_Record";
+            this.btn_Record.Size = new System.Drawing.Size(87, 46);
+            this.btn_Record.TabIndex = 1;
+            this.btn_Record.UseVisualStyleBackColor = true;
+            this.btn_Record.Click += new System.EventHandler(this.Btn_Record_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(115, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(736, 281);
+            this.textBox1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(358, 414);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(238, 37);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Indigo;
+            this.label1.Location = new System.Drawing.Point(266, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Скорость:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(863, 547);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl_Top);
+            this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.pnl_Top.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Top;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Record;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_openfile;
+        private System.Windows.Forms.Button btn_resume;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace New_Project_Voice
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer speech;
+        public Form1()
+        {
+            InitializeComponent();
+            Form_Location();
+            btn_Close.Click += (s, e) => { Close(); };
+        }
+        void Form_Location()
+        {
+            int move = 0, moveX = 0, moveY = 0;
+            pnl_Top.MouseDown += (s, e) => { move = 1; moveX = e.X; moveY = e.Y; };
+            pnl_Top.MouseMove += (s, e) => { if (move == 1) SetDesktopLocation(MousePosition.X - moveX, MousePosition.Y - moveY); };
+            pnl_Top.MouseUp += (s, e) => { move = 0; };
+        }
+
+        void Btn_start_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(textBox1.Text)){
+
+                speech = new SpeechSynthesizer();
+                speech.Rate = trackBar1.Value;
+                speech.SpeakAsync(textBox1.Text);
+            }
+            else
+                MessageBox.Show("Напиши что-нибудь!", "Уведомление системы!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        void Btn_pause_Click(object sender, EventArgs e)
+        {
+            speech.Pause();
+        }
+
+        void Btn_resume_Click(object sender, EventArgs e)
+        {
+            speech.Resume();
+        }
+
+        void Btn_openfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Filter = "text files|*.txt"
+            };
+            openFile.ShowDialog();
+            string filename = openFile.FileName;
+            StreamReader stream = new StreamReader(filename);
+            textBox1.Text = stream.ReadToEnd();
+            stream.Close();
+        }
+
+        void Btn_Record_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog
+            {
+                Filter = "Wave Files| *.wav"
+            };
+            saveFile.ShowDialog();
+            string filename;
+            filename = saveFile.FileName;
+            speech = new SpeechSynthesizer();
+            speech.Rate = trackBar1.Value;
+            speech.SetOutputToWaveFile(filename);
+            speech.Speak(textBox1.Text);
+            speech.SetOutputToDefaultAudioDevice();
+            MessageBox.Show("Аудиофайл успешно сохранен!");
+        }
+    }
+}
